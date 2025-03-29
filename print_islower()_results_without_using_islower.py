963241a6362718281a3d0da25cbbaee3a99144ee
Matthew Illac D. Sosa <matthewillacsosa@gmail.com>
2025-03-29 12:49:24 +0800

    jus tlike my previous program on isupper() i used islower() then but now i did it vice versa to check if my input here is a lower cased type, in short, vice versa'ed the program and making it work for the opposite.

--- print_islower()_results_without_using_islower.py
+++ print_islower()_results_without_using_islower.py
@@ -1,3 +1,10 @@
 #Prog04. islower() check if all characters of the string is on lower case. Create a program that do the same functionality without using islower() function.
+def low_taper_check(string):
+    for char_checker in string:
+        if char_checker.isupper():
+            return False
+    return True
 #enter "help"
+text = input("Enter a string: ")
 #print "true"
+print(low_taper_check(text))
