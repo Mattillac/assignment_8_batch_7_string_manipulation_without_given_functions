38c18b0efa22e368c85a82f00e9d8b591dd0dc27
Matthew Illac D. Sosa <matthewillacsosa@gmail.com>
2025-03-29 13:06:13 +0800

    since imy previous work on endswith() i used a longer code but this time i found a better one tahnks to w3schools i found a similar func and decided to create a program that acts like this using find() to check if my entered word is matching the str in text

--- print_startswith()_results_without_using_startswith.py
+++ print_startswith()_results_without_using_startswith.py
@@ -1,3 +1,7 @@
 #Prog05. startswith() check if the string beginning part matches the function parameter. Create a program that do the same functionality without using startswith() 
+text = input("Enter a word: ")
+start_checker = input("Enter the word to check: ")
 #enter "FOR DEMACIA"
+if text.find(start_checker) == 0:
+    print(True)
 #print true
