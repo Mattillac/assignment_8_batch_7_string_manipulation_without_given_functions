ec92de507e93c78cc53df40866c9576b4b8320ec
Matthew Illac D. Sosa <matthewillacsosa@gmail.com>
2025-03-29 11:17:09 +0800

    here is my final code for program 1 as you can see i used a while loop to constantly check the space detector i set, and as it constantly removes any spaes nearby the string i entered using the endswith(0 function

--- print_rstrip()_results_without_using_rstrip.py
+++ print_rstrip()_results_without_using_rstrip.py
@@ -1,3 +1,7 @@
 #Prog01. rstrip() remove the space characters at the end of the string. Create a program that do the same functionality without using rstrip() function.
+text = input("Enter a string: ")
 #enter "FOR DEMACIA!   "
+while text.endswith(" "):
+    text = text[:-1]
 #print "FOR DEMACIA!"
+print(text)
