0a7fada7e29e4c9577929d3f976ab158f82b959b
Matthew Illac D. Sosa <matthewillacsosa@gmail.com>
2025-03-29 11:46:53 +0800

    here is my code for program 2 as you can see used splicing again and i used endswith again since it has an almost same like function with removesuffix but this time i used if statement and i valued only a specific input to remove, since the main ogla only is to create a program that runs the same function but without using the removesuffix()

--- print_removesuffix()_results_without_using_removesuffix.py
+++ print_removesuffix()_results_without_using_removesuffix.py
@@ -1,3 +1,8 @@
 #Prog02. removesuffix() remove the characters at the end of the string that matches the function parameter. Create a program that do the same functionality without using removesuffix() function.
+text = "doom.exe"
+suffix = ".exe"
 #example "doom.exe"
+if text.endswith(suffix):
+        text = text[:-len(suffix)]
 #print "doom"
+print(text)
