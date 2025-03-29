7e467779776bdae8fc48886e104cfcfb407d964a
Matthew Illac D. Sosa <matthewillacsosa@gmail.com>
2025-03-29 11:01:11 +0800

    here is my pseudocode for program 1

---
+++ print_rstrip()_results_without_using_rstrip.py
@@ -0,0 +1,3 @@
+#Prog01. rstrip() remove the space characters at the end of the string. Create a program that do the same functionality without using rstrip() function.
+#enter "FOR DEMACIA!   "
+#print "FOR DEMACIA!"
