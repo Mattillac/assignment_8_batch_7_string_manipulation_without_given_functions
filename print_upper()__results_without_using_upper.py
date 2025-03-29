4e212e7e4b691846f508bcd9480b7484a9d19db1
Matthew Illac D. Sosa <matthewillacsosa@gmail.com>
2025-03-29 12:08:28 +0800

    jus tlike my previous program on batch 6 on my lower program i used this str.maketrans again so the pool of cap and lower cased alpahabets will be translated thanks to the translate function. Printing the results

--- print_upper()__results_without_using_upper.py
+++ print_upper()__results_without_using_upper.py
@@ -1,3 +1,10 @@
 #Prog03. upper() converts all characters of the string into upper case. Create a program that do the same functionality without using upper() function.
+def fixer_upper(text):
+    upper_cut = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    lower_cut = "abcdefghijklmnopqrstuvwxyz"
+    final_cut = str.maketrans(lower_cut, upper_cut)
+    return text.translate(final_cut)
 # enter "darkness"
+text = input("Enter a string: ")
 #print "DARKNESS"
+print(fixer_upper(text))
