32c0223ef84e53d59b1a3ac27c1e65f8bf2112a3
Matthew Illac D. Sosa <matthewillacsosa@gmail.com>
2025-03-29 11:28:47 +0800

    here is my pseudocode for program 2

---
+++ print_removesuffix()_results_without_using_removesuffix.py
@@ -0,0 +1,3 @@
+#Prog02. removesuffix() remove the characters at the end of the string that matches the function parameter. Create a program that do the same functionality without using removesuffix() function.
+#example "doom.exe"
+#print "doom"
