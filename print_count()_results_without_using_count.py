ad14c432cf39d556d6ca1eac36478abe3f913aeb
Matthew Illac D. Sosa <matthewillacsosa@gmail.com>
2025-03-31 01:01:32 +0800

    here i pulled a trick on my sleeve using the collections and importing the built in counter to literally count elements in the input and the import itself is self explanatory and yeah it wokrs using one swoop

--- print_count()_results_without_using_count.py
+++ print_count()_results_without_using_count.py
@@ -1,3 +1,8 @@
 #Prog08. count() return how many time the function parameter appear in the string. Create a program that do the same functionality without using count() function.
+from collections import Counter
 #read "Mushroooms!"
+text = input("Enter a random word or number: ")
+your_target = input("Enter the character to count: ")
+counter_texterists = Counter(text)
 #print "there are 3 os in "Mushroooms!"
+print(counter_texterists[your_target])
