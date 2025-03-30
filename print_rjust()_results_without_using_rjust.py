66260a155582b8401dc1e9e1a5aa3d64a8d3ccaf
Matthew Illac D. Sosa <matthewillacsosa@gmail.com>
2025-03-30 23:34:30 +0800

    HER EIS MY PESUDOCODE FOR PROGRAM 6

---
+++ print_rjust()_results_without_using_rjust.py
@@ -0,0 +1,3 @@
+#Prog06. rjust() add space characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using rjust() function.
+#enter "CAPTAIN!"
+#PRINT "    CAPTAIN!"
