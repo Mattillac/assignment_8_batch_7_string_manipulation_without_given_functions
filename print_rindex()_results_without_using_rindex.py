7bd64962fafe649950111285d23c27a6d92c0fc5
Matthew Illac D. Sosa <matthewillacsosa@gmail.com>
2025-03-31 02:14:17 +0800

    as you can see since it's the same as my last program but with a twist of its counting backwards check line 7, i put a  bunch of negative ones so that it'll count backwards and moves one step backwards just like i said until it reaches its goal, now this is hwere line 8-10 come to work.  And it stores it once the program found it and break and print results!

--- print_rindex()_results_without_using_rindex.py
+++ print_rindex()_results_without_using_rindex.py
@@ -1,3 +1,13 @@
 #Prog10. rindex() return the first location of the function parameter in the string starting from the last character. Create a program that do the same functionality without using rindex() function.
 #EXAMPLE "Green"
+text = input("Enter a word: ")
+goal = input("Enter what to find where and when it starts: ")
+index_bootleg = -1  
+
+for index_occurence in range(len(text) - 1, -1, -1):  
+    if text[index_occurence:index_occurence + len(goal)] == goal:
+        index_bootleg = index_occurence
+        break  
+
+print(index_bootleg)
 #print "the last E is at index 3"
