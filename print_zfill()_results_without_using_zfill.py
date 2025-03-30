c6b8cb891b0bc2ab0d7961dcc4d7c45dfc012592
Matthew Illac D. Sosa <matthewillacsosa@gmail.com>
2025-03-31 00:23:05 +0800

    here as you can see i used string formatting as i preset the text = f"{text:0>{length}}" where in {} the operation happens, witht he jhelp of : this where it starts formatting and fill it using 0s and > making it fill right align. adn the rest is one swoop and print the results

--- print_zfill()_results_without_using_zfill.py
+++ print_zfill()_results_without_using_zfill.py
@@ -1,3 +1,7 @@
 #Prog07. zfill() add zero characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using zfill() function.
 #enter "123"
+text = int(input("Enter 3 numbers: "))
+length = int(input("Enter the length of the string: "))
+text = f"{text:0>{length}}"
 #print "000123"
+print(text)
