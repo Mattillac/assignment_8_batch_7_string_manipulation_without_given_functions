29fe3a03e3e9d65699bcc2986f5e8d16c5551880
Matthew Illac D. Sosa <matthewillacsosa@gmail.com>
2025-03-31 01:55:11 +0800

    as you can see here i used a for loop instead of find() i wanna use find() but i find it that it doesnt really follow the given instructions? so yes, i created a for loop. here you can see that it runs each characters you typed are being read and once you set the goal it will find it and it'll stop and thats what the line 7-9 do  while on line 10 it will stop and it prints the result

--- print_index()_results_without_using_index.py
+++ print_index()_results_without_using_index.py
@@ -1,3 +1,13 @@
 #Prog09. index() return the first location of the function parameter in the string. Create a program that do the same functionality without using index() function.
+text = input("Enter a string: ")
+goal = input("Enter what to find where and when it starts: ")
 #EXAMPLE: "Purple"
+index_bootleg = -1  
+
+for index_occurence in range(len(text)):
+    if text[index_occurence] == goal:
+        index_bootleg = index_occurence
+        break 
+
 #print "2" (because "r" is at index 2)
+print(index_bootleg)
