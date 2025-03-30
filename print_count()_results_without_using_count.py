f48d21b42f57bfdeac3dae572f533da8eb441c09
Matthew Illac D. Sosa <matthewillacsosa@gmail.com>
2025-03-31 00:29:58 +0800

    here is my pseudocode for program 8

---
+++ print_count()_results_without_using_count.py
@@ -0,0 +1,3 @@
+#Prog08. count() return how many time the function parameter appear in the string. Create a program that do the same functionality without using count() function.
+#read "Mushroooms!"
+#print "there are 3 os in "Mushroooms!"
